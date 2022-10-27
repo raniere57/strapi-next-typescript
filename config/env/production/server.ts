d2712d82142a61fed5698557bf61c7bbde84175d
Raniere Rodrigues Gomes <raniere57@icloud.com>
2022-10-27 19:48:01 -0300

    fixed bugs on server.ts

--- config/env/production/server.ts
+++ config/env/production/server.ts
@@ -2,6 +2,6 @@ export default ({ env }) => ({
   url: env('https://strapi-next-typescript.herokuapp.com/'),
   proxy: true,
   app: {
-    keys: env.array('APP_KEYS', [pVl2XLF/aCawWRuhz3vZ0Q==,/yJC1dNpO0lOrbuaYJUW0A==,+ZTECs6Dhgui2RILuC5Qug==,jRrooO+c8I0sT4D9+aXvCA==])
+    keys: env.array('APP_KEYS', ['pVl2XLF/aCawWRuhz3vZ0Q==','/yJC1dNpO0lOrbuaYJUW0A==','+ZTECs6Dhgui2RILuC5Qug==','jRrooO+c8I0sT4D9+aXvCA=='])
   }
 })
