d793ee45f01de76c1e77e8e32c7fb378bd8bedd1
Raniere Rodrigues Gomes <raniere57@icloud.com>
2022-10-28 15:03:44 -0300

    trying something

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,6 +1,28 @@
 export default [
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'res.cloudinary.com', // cloudinary images
+            'lh3.googleusercontent.com', // google avatars
+            'platform-lookaside.fbsbx.com', // facebook avatars
+            'dl.airtable.com', // strapi marketplace
+            `https://${env('AWS_BUCKET')}.s3.${env('AWS_REGION')}.amazonaws.com`
+          ],
+          'media-src': ["'self'", 'data:', 'blob:', `https://${env('AWS_BUCKET')}.s3.${env('AWS_REGION')}.amazonaws.com`],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
@@ -9,4 +31,8 @@ export default [
   'strapi::session',
   'strapi::favicon',
   'strapi::public',
-];
+]
+function env(arg0: string) {
+  throw new Error('Function not implemented.')
+}
+
